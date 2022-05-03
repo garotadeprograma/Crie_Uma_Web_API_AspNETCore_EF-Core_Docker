baf5ab68c2eecca23f8ed05dc924bd9d3ba0b299
anna.silva <anna.silva@ituan.com.br>
2022-05-02 22:06:50 -0300

    Add propriedades à classe Aluno

--- SmartSchool.WebApi/Models/Aluno.cs
+++ SmartSchool.WebApi/Models/Aluno.cs
@@ -2,6 +2,22 @@ namespace SmartSchool.WebApi.Models
 {
     public class Aluno
     {
+        public Aluno() {}
+
+        public Aluno(int Id, string Nome, string SobreNome, string Telefone)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.SobreNome = SobreNome;
+            this.Telefone = Telefone;
+        }
         
+        public int Id { get; set; }        
+
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        public string Telefone { get; set; }
     }
 }
