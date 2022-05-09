17d599aa2f9be6db977104f15409dae2da359b72
anna.silva <anna.silva@ituan.com.br>
2022-05-08 23:43:47 -0300

    Add POST method

--- SmartSchool.WebApi/Controllers/AlunoController.cs
+++ SmartSchool.WebApi/Controllers/AlunoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SmartSchool.WebApi.Data;
+using SmartSchool.WebApi.Models;
 
 namespace SmartSchool.WebApi.Controllers
 {
@@ -20,6 +21,30 @@ namespace SmartSchool.WebApi.Controllers
 
         [HttpGet]
         public IActionResult Get()
+        {
+            return Ok(_smartContext.Alunos);
+        }
+
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int Id)
+        {
+            var aluno = _smartContext.Alunos.FirstOrDefault(a => a.Id == Id);
+            if (aluno == null) return BadRequest("Id não corresponde ao de nenhum aluno!");
+            
+            return Ok(aluno);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome)
+        {
+            var alunos = _smartContext.Alunos.Where(a => a.Nome.Contains(nome) || a.SobreNome.Contains(nome));
+
+            if(alunos == null) return BadRequest("Não há nenhum aluno com o nome informado");
+            return Ok(alunos);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
         {
             return Ok(_smartContext.Alunos);
         }
