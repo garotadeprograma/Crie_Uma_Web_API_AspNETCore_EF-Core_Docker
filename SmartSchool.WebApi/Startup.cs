c5203af03b065708377b2c6530c92799ad9bf228
anna.silva <anna.silva@ituan.com.br>
2022-05-11 10:05:46 -0300

    repository injected on the starup class

--- SmartSchool.WebApi/Startup.cs
+++ SmartSchool.WebApi/Startup.cs
@@ -27,12 +27,13 @@ namespace SmartSchool.WebApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            // services.AddDbContext<SmartContext>(context => context.UseSqlServer(Configuration.GetConnectionString("Default")));
+        {        
+            services.AddScoped<IRepository, Repository>();
+
             services.AddDbContext<SmartContext>(
                 context => context.UseSqlServer(Configuration.GetConnectionString("Default"))
             );  
+            services.AddControllers();
 
         }
 
